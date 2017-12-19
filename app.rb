e6cb256668f5439f8be5858e826777293320a577
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-19 20:22:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,15 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Hello, World!"
+  end 
+  
   get '/name' do
     "My name is Matija"
   end
 
-  get '/hometown' do 
+  get '/hometown' do
     "My hometown is Ljubljana"
   end
 
-  get '/favorite_song' do 
+  get '/favorite_song' do
     "My favorite song is the Hills"
-  end  
+  end
 end
